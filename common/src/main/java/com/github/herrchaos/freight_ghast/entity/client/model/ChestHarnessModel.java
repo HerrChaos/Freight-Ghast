43be3258229dd5fbce50a4b6b9f59ddc5fcccf59
HerrChaos <138036657+HerrChaos@users.noreply.github.com>
2025-11-10 12:13:22 +0100

    1.21.10

--- common/src/main/java/com/github/herrchaos/freight_ghast/entity/client/model/ChestHarnessModel.java
+++ common/src/main/java/com/github/herrchaos/freight_ghast/entity/client/model/ChestHarnessModel.java
@@ -5,10 +5,11 @@ import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
 
 import java.util.List;
 
-public class ChestHarnessModel extends Model {
+public class ChestHarnessModel<T extends LivingEntityRenderState> extends Model<T> {
     private final ModelPart holder;
     private final ModelPart holster5;
     private final ModelPart holster3;
--- common/src/main/java/com/github/herrchaos/freight_ghast/entity/client/renderlayer/ChestRenderLayer.java
+++ common/src/main/java/com/github/herrchaos/freight_ghast/entity/client/renderlayer/ChestRenderLayer.java
@@ -5,30 +5,29 @@ import com.github.herrchaos.freight_ghast.datasaver.HappyGhastRenderStateDataSav
 import com.github.herrchaos.freight_ghast.entity.client.ModModelLayers;
 import com.github.herrchaos.freight_ghast.entity.client.model.ChestHarnessModel;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.HappyGhastModel;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.entity.state.HappyGhastRenderState;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 
 public class ChestRenderLayer<M extends HappyGhastModel> extends RenderLayer<HappyGhastRenderState, M> {
     private static final ResourceLocation GHAST_CHEST_HOLSTER = FreightGhast.idOf("textures/entity/ghast/chest_harness.png");
 
-    private final ChestHarnessModel model;
+    private final ChestHarnessModel<HappyGhastRenderState> model;
 
     public ChestRenderLayer(RenderLayerParent<HappyGhastRenderState, M> renderLayerParent, EntityModelSet entityModelSet) {
         super(renderLayerParent);
-        this.model = new ChestHarnessModel(entityModelSet.bakeLayer(ModModelLayers.CHEST_HARNESS));
+        this.model = new ChestHarnessModel<>(entityModelSet.bakeLayer(ModModelLayers.CHEST_HARNESS));
     }
 
     @Override
-    public void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int light, HappyGhastRenderState entityRenderState, float f, float g) {
+    public void submit(PoseStack poseStack, SubmitNodeCollector submitNodeCollector, int light, HappyGhastRenderState entityRenderState, float f, float g) {
         if (((HappyGhastRenderStateDataSaver) entityRenderState).freight_ghast$getChestAmount() > 0) {
 
             for (int i = 0; i < this.model.getHolsters().size(); i++) {
@@ -40,9 +39,7 @@ public class ChestRenderLayer<M extends HappyGhastModel> extends RenderLayer<Hap
 
             poseStack.pushPose();
 
-            VertexConsumer vertexConsumer = multiBufferSource.getBuffer(RenderType.entityCutout(GHAST_CHEST_HOLSTER));
-            int j = LivingEntityRenderer.getOverlayCoords(entityRenderState, 0.0F);
-            this.model.renderToBuffer(poseStack, vertexConsumer, light, j);
+            submitNodeCollector.submitModel(this.model, entityRenderState, poseStack, RenderType.entityCutout(GHAST_CHEST_HOLSTER), light, OverlayTexture.NO_OVERLAY, entityRenderState.outlineColor, null);
 
             poseStack.popPose();
         }
